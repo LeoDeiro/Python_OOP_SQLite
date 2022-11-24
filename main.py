a5a2684730d1da52e6c1bf2ecc85678632233d75
Snake <64919417+LeoDeiro@users.noreply.github.com>
2022-11-24 01:04:41 +0000

    Database

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+#Importa nosso arquivo Pessoa.py no diretório model
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
-pessoa1 = Pessoa(1 , "Pessoa Um")
-print(pessoa1)
+#Exemplo de uso
+poyatos = Pessoa(1, "Henrique Poyatos")
+print(poyatos)
 
+#Quero mostrar só o nome
+print(poyatos.nome)
+print(45*("-"))
+#Chama o objeto de banco de dados
+db = Database()
+
+#Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Carregar lista do banco de dados
+pessoas = pessoaDAO.getAll(True)
+
+for pessoa in pessoas:
+  print(pessoa)
